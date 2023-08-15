f6d629aca6ee68391dd9a53964f53a7c52a3c500
chetanguptaa <chetangupta123raj@gmail.com>
2023-08-15 12:49:07 +0530

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div>
-      Hello world
+      <p>Homepage</p>
+      <hr />
+      <Link href="/contact" className="m-5">Contact Me</Link>
+      <Link href="/projects" className="m-5">Projects and Skills</Link>
     </div>
   )
 }
