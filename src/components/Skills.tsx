fdc46387f4474bdcde68f14ca348043cf030279d
chetan gupta <chetangupta123raj@gmail.com>
2023-08-18 23:22:35 +0530

    updated Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import SectionHeading from "./SectionHeading";
-import { skillsData } from "@/lib/data";
+import { skillsData, familiarWithSkillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 
@@ -24,10 +24,11 @@ export default function Skills() {
   const { ref } = useSectionInView("Skills");
 
   return (
+    <>
     <section
       id="skills"
       ref={ref}
-      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+      className="mb-12 max-w-[53rem] scroll-mt-28 text-center"
     >
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
@@ -48,5 +49,30 @@ export default function Skills() {
         ))}
       </ul>
     </section>
+    <section
+    id="familiarWithSkills"
+    ref={ref}
+    className="max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+  >
+    <SectionHeading>Also familiar with</SectionHeading>
+    <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+      {familiarWithSkillsData.map((skill, index) => (
+        <motion.li
+          className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+          key={index}
+          variants={fadeInAnimationVariants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+          custom={index}
+        >
+          {skill}
+        </motion.li>
+      ))}
+    </ul>
+  </section>
+    </>
   );
 }
