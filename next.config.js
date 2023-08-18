c73b42b4fcba9a74fb9751c1b04d435e7f1a5cf7
chetan gupta <chetangupta123raj@gmail.com>
2023-08-18 23:20:56 +0530

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-        },
-      ],
-    },
     experimental: {
       serverActions: true,
     },
-  };
+};
   
-  module.exports = nextConfig;
+module.exports = nextConfig;
