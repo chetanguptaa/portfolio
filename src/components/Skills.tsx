4389633b761a8ba0966f2b3e5f92b5e32ea6b40a
chetan gupta <chetangupta123raj@gmail.com>
2023-08-20 13:18:01 +0530

    fixed a bug

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -25,54 +25,54 @@ export default function Skills() {
 
   return (
     <>
-    <section
-      id="skills"
+      <section
+        id="skills"
+        ref={ref}
+        className="mb-12 max-w-[53rem] scroll-mt-28 text-center"
+      >
+        <SectionHeading>My skills</SectionHeading>
+        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+          {skillsData.map((skill, index) => (
+            <motion.li
+              className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        </ul>
+      </section>
+      <section
+      id="familiarWithSkills"
       ref={ref}
-      className="mb-12 max-w-[53rem] scroll-mt-28 text-center"
-    >
-      <SectionHeading>My skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
-          <motion.li
-            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-            key={index}
-            variants={fadeInAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: true,
-            }}
-            custom={index}
-          >
-            {skill}
-          </motion.li>
-        ))}
-      </ul>
-    </section>
-    <section
-    id="familiarWithSkills"
-    ref={ref}
-    className="mb-12 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
-  >
-    <SectionHeading>Also familiar with</SectionHeading>
-    <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-      {familiarWithSkillsData.map((skill, index) => (
-        <motion.li
-          className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-          key={index}
-          variants={fadeInAnimationVariants}
-          initial="initial"
-          whileInView="animate"
-          viewport={{
-            once: true,
-          }}
-          custom={index}
-        >
-          {skill}
-        </motion.li>
-      ))}
-    </ul>
-  </section>
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+      >
+        <SectionHeading>Also familiar with</SectionHeading>
+        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+          {familiarWithSkillsData.map((skill, index) => (
+            <motion.li
+              className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
