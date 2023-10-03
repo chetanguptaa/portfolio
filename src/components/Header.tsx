2af5ba3c8ce5e6ad78a66c6b17cba7e45ef58f09
chetan gupta <chetangupta123raj@gmail.com>
2023-10-03 13:23:22 +0530

    updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex w-[22rem] items-center justify-center gap-y-0.5 sm:gap-y-0 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] ml-4 mr-4 sm:gap-5 sm:flex-nowrap">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
@@ -58,6 +58,34 @@ export default function Header() {
               </Link>
             </motion.li>
           ))}
+          <motion.li
+            className="h-3/4 flex items-center justify-center relative"
+            key={5}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+          >
+            <Link
+              className={clsx(
+                "flex items-center justify-center pr-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+              )}
+              href={"/blogs"}
+              onClick={() => {
+                setTimeOfLastClick(Date.now());
+              }}
+            >
+              {"Blogs"}
+              {"Blogs" && (
+                <motion.span
+                  className="rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                  transition={{
+                    type: "spring",
+                    stiffness: 380,
+                    damping: 30,
+                  }}
+                ></motion.span>
+              )}
+            </Link>
+          </motion.li>
         </ul>
       </nav>
     </header>
