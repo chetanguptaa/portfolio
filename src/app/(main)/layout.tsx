e4987a4e07ca5017ff72ccfa3cbbd7e1b9329e55
chetan gupta <chetangupta123raj@gmail.com>
2023-10-03 13:25:26 +0530

    moved app layout into (app)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
-import "./globals.css";
+"use client";
+
 import { Inter } from "next/font/google";
-import Header from "../components/Header";
-import "./globals.css";
+import Header from "@/components/Header";
+import "../globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
-import Footer from "../components/Footer";
+import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
+import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
   title: "Chetan Gupta",
@@ -22,12 +24,12 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className="bg-[#6495ED] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#867dcf]"></div>
+        <div className="bg-[#867dcf] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#6495ED]"></div>
 
         <ActiveSectionContextProvider>
           <Header />
-          {children}
+          <SessionProvider>{children}</SessionProvider>
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
