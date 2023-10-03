d295f1ef4b88e566a4e9d138066944dea41561ab
chetan gupta <chetangupta123raj@gmail.com>
2023-10-03 13:22:23 +0530

    added rich text editor

---
+++ src/components/editor/TipTapMenuBar.tsx
@@ -0,0 +1,128 @@
+import { Editor } from "@tiptap/react";
+import {
+  AiFillCodepenCircle,
+  AiOutlineOrderedList,
+  AiOutlineUnorderedList,
+  AiOutlineRedo,
+  AiOutlineUndo,
+} from "react-icons/ai";
+import { BsCodeSlash, BsQuote } from "react-icons/bs";
+import { BiBold, BiStrikethrough } from "react-icons/bi";
+import {
+  LuHeading1,
+  LuHeading2,
+  LuHeading3,
+  LuHeading4,
+  LuHeading5,
+  LuHeading6,
+} from "react-icons/lu";
+import { FiItalic } from "react-icons/fi";
+
+const TipTapMenuBar = ({ editor }: { editor: Editor }) => {
+  return (
+    <div className="flex flex-wrap gap-2">
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
+        <BiBold className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
+      >
+        <FiItalic className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
+        <BiStrikethrough className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        disabled={!editor.can().chain().focus().toggleCode().run()}
+        className={editor.isActive("code") ? "is-active" : ""}
+      >
+        <BsCodeSlash className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+      >
+        <LuHeading1 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+      >
+        <LuHeading2 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+      >
+        <LuHeading3 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
+      >
+        <LuHeading4 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
+      >
+        <LuHeading5 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
+      >
+        <LuHeading6 className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
+      >
+        <AiOutlineUnorderedList className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive("orderedList") ? "is-active" : ""}
+      >
+        <AiOutlineOrderedList className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive("codeBlock") ? "is-active" : ""}
+      >
+        <AiFillCodepenCircle className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive("blockquote") ? "is-active" : ""}
+      >
+        <BsQuote className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        <AiOutlineUndo className="w-6 h-6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        <AiOutlineRedo className="w-6 h-6" />
+      </button>
+    </div>
+  );
+};
+
+export default TipTapMenuBar;
