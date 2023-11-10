224473e4d74dc69ab4c550af2f9724e1cb78bcbd
chetan gupta <chetangupta123raj@gmail.com>
2023-11-10 17:16:54 +0530

    fix: Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full "
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -30,7 +30,7 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ",
+                  "flex w-full items-center justify-center px-[0.9vh] py-3 hover:text-gray-950 transition",
                   {
                     "text-gray-950 ": activeSection === link.name,
                   }
@@ -65,7 +65,7 @@ export default function Header() {
           >
             <Link
               className={clsx(
-                "flex items-center justify-center pr-3 hover:text-gray-950 transition "
+                "flex items-center justify-center hover:text-gray-950 transition pr-[0.9vh]"
               )}
               href={"/blogs"}
               onClick={() => {
