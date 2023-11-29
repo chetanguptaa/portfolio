ff96bd0777970222146180d55d5fd1f05743eafa
chetan gupta <chetangupta123raj@gmail.com>
2023-11-29 13:43:18 +0530

    updated Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -13,7 +13,6 @@ export default function Project({
   description,
   tags,
   imageUrl,
-  githubUrl,
 }: ProjectProps) {
   const router = useRouter();
   const ref = useRef<HTMLDivElement>(null);
@@ -55,7 +54,6 @@ export default function Project({
           alt="Project I worked on"
           quality={95}
           priority
-          onClick={() => router.push(githubUrl)}
           className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition  group-hover:scale-[1.04] group-hover:-translate-x-3
           group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3
           group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
