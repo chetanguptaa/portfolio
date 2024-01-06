45514b2ba6f4f861b2d77c7aa34b0b74c954e4c6
chetan gupta <chetangupta123raj@gmail.com>
2024-01-06 14:48:55 +0530

    fix: blog layout

--- src/app/(blogs)/blogs/[id]/page.tsx
+++ src/app/(blogs)/blogs/[id]/page.tsx
@@ -60,26 +60,24 @@ const BlogPage = ({ params }: { params: { id: string } }) => {
         className={`max-w-3xl m-auto text-2xl font-medium text-black bg-white ${roboto.className}`}
       >
         <div className="md:text-justify p-4 md:p-8 flex flex-col justify-center">
-          <div className="mb-8">
-            <div className="text-black font-bold text-2xl md:text-3xl mb-2 text-center underline">
-              {blog.title.toUpperCase()}
-            </div>
-            <div className="text-sm mr-2 mb-12">
-              <p className="text-sm flex float-left">
-                <AiOutlineRead className="mr-2 mt-1" />
-                {Math.round(
-                  Number(calculateReadingTime(blog.editorState).toFixed(1))
-                )}{" "}
-                min read
-              </p>
-              <p className="flex float-right text-sm text-rose-500">
-                <AiOutlineStar className="mr-2 float-right mt-1" />
-                {calculateDate(blog.createdAt.toString())}
-              </p>
-            </div>
-            <div className="prose max-w-full text-base font-medium">
-              {ReactHtmlParser(blog?.editorState)}
-            </div>
+          <div className="text-black font-bold text-2xl md:text-3xl mb-2 text-center underline">
+            {blog.title.toUpperCase()}
+          </div>
+          <div className="text-sm flex justify-between mb-12">
+            <p className="text-sm flex">
+              <AiOutlineRead className="mr-2 mt-1" />
+              {Math.round(
+                Number(calculateReadingTime(blog.editorState).toFixed(1))
+              )}{" "}
+              min read
+            </p>
+            <p className="flex text-sm text-rose-500">
+              <AiOutlineStar className="mr-2 float-right mt-1" />
+              {calculateDate(blog.createdAt.toString())}
+            </p>
+          </div>
+          <div className="prose max-w-full text-base font-medium">
+            {ReactHtmlParser(blog?.editorState)}
           </div>
         </div>
       </div>
