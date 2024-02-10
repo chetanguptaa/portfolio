899638c29da4dfba847b6416ae7d93e07393045a
chetan gupta <chetanguptaaaa21@gmail.com>
2024-02-10 15:11:09 +0530

    fix: changed font to mulish

--- src/app/(blogs)/layout.tsx
+++ src/app/(blogs)/layout.tsx
@@ -1,10 +1,8 @@
-import { Montserrat } from "next/font/google";
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400"],
-});
 import "../globals.css";
 import { Metadata } from "next";
+import { Mulish } from "next/font/google";
+
+const mulish = Mulish({ subsets: ["latin"], weight: "300" });
 
 export const metadata: Metadata = {
   title: "Chetan Gupta | Blogs",
@@ -18,7 +16,7 @@ export default function BlogsLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${montserrat.className} text-gray-950 relative `}>
+      <body className={`${mulish.className} text-gray-950 relative `}>
         {children}
       </body>
     </html>
