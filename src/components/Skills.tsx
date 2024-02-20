5ad0bae67b74c0340f333f35cb50abe56519ca6f
chetan gupta <chetanguptaaaa21@gmail.com>
2024-02-20 12:47:21 +0530

    fix: render loop

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeading from "./SectionHeading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
@@ -53,7 +53,7 @@ export default function Skills() {
           <div className="grid sm:grid-cols-4 grid-cols-2 justify-center gap-4 text-lg text-gray-800">
             {displaySkills().map((skill, index) => (
               <motion.div
-                className="bg-blue-100 border-blue-400 border hover:bg-blue-200 hover:shadow-lg rounded-xl px-5 py-3 font-semibold flex items-center justify-center transition duration-300 ease-in-out transform hover:-translate-y-1"
+                className="bg-blue-100 border-blue-400 border hover:bg-blue-200 hover:shadow-lg rounded-xl sm:px-5 sm:py-3 p-2 font-semibold flex items-center justify-center transition duration-300 ease-in-out transform hover:-translate-y-1"
                 key={startIndex + index}
                 variants={fadeInAnimationVariants}
                 initial="initial"
